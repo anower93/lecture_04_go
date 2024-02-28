cab7d3d971d03196a1ba90ff5477c088d84eff13
Anower_Hoshin <anowerhossin1993@gmail.com>
2024-02-29 01:56:27 +0600

    Everything about for loop

--- main.go
+++ main.go
@@ -1,9 +1,40 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 
 	//Loop, In Go programming it has only one loop
 
-	
+	//Simple for loop
+	for i := 1; i <= 9; i++ {
+		fmt.Println(i)
+
+	}
+
+	// Range in for loop
+
+	students := [...]string{"One", "Two", "Three"}
+
+	for i, std := range students {
+		fmt.Println(i, std)
+	}
+
+	//For while loop
+
+	x := 0
+	for x < 11 {
+		fmt.Println(x, "Hello")
+		x++
+	}
+
+	// For infinity
+
+	for {
+		fmt.Println("Hello")
+		x++
+	}
 
 }
